90773428a7f9ee6d63319ce0832792c698f2373a
danieloest <danieloest@users.noreply.github.com>
2019-05-01 14:55:39 -0600

    adding ajax functions

--- web/ta02/login.php
+++ web/ta02/login.php
@@ -8,23 +8,29 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="superGlass.css">
+  <!-- Add jQuery -->
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <title>Login</title>
 </head>
 <body>
   
   <?php include 'header.php';
   ?>
-  <button onclick="loginAdmin()">Login as Administrator</button>
-  <button onclick="loginTester()">Login as tester</button>
+  <button id="loginAdmin" onclick="loginAdmin()">Login as Administrator</button>
+  <button id="loginTester" onclick="loginTester()">Login as tester</button>
 
-  <?php
-  function loginAdmin() {
-    echo "<br><h2>Test. Logged in as admin<h2>";
-  }
-  function loginTester() {
-    echo "<br><h2>Test. Logged in as Tester<h2>";
-  }
-  ?>
-  
+  <script>
+    $("#loginAdmin").click(function(){
+      alert("Clicked!");
+    }
+
+    );
+  // function loginAdmin() {
+    
+  // }
+  // function loginTester() {
+
+  // }
+  </script>
 </body>
 </html>
