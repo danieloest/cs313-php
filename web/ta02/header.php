b76c151b73b5ce7b34f245a7f48fff0382b3dafa
danieloest <danieloest@users.noreply.github.com>
2019-04-30 19:01:08 -0600

    Adding header.php

---
+++ web/ta02/header.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+<header>
+<nav>
+  <ul>
+    <li><a href=\"about-us.php\">About us</a></li>
+    <li><a href="."login.php".">Login</a></li>
+    <li><a href="."home.php".">Home</a></li>
+    <ul>
+  
+  </nav>
+</header>";
+  
+?>
