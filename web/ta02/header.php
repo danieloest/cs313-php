c9775207bd7451de19c933e10757cda3c8785173
danieloest <danieloest@users.noreply.github.com>
2019-04-30 19:12:30 -0600

    Adding changes

--- web/ta02/header.php
+++ web/ta02/header.php
@@ -1,10 +1,12 @@
 <?php
+// Get the current title of the page
 $activePage = basename($_SERVER['PHP_SELF'], ".php");
 
 ?>
 <header>
 <nav>
   <ul>
+    <!-- If $activePage is the title of the page, the link's class is "active". Else the class is "" -->
     <li class="<?= ($activePage == 'about-us') ? 'active':''; ?>"><a href="about-us.php">About us</a></li>
     <li class="<?= ($activePage == 'login') ? 'active':''; ?>"><a href="login.php">Login</a></li>
     <li class="<?= ($activePage == 'home') ? 'active':''; ?>"><a href="home.php">Home</a></li>
