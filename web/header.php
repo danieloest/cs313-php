c6a7147f03923ea9b9b89a0527a338d851ceea83
danieloest <danieloest@users.noreply.github.com>
2019-05-02 16:07:12 -0600

    Adding Bootstrap

---
+++ web/header.php
@@ -0,0 +1,3 @@
+<div class="jumbotron">
+    <h1>Welcome to my website!</h1>
+</div>
--- web/index.php
+++ web/index.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Add Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <title>Document</title>
 </head>
 <body>
