e9ae0c361fec939d4fb573caf18097a93ba89793
danieloest <danieloest@users.noreply.github.com>
2019-05-03 23:04:48 -0600

    Adding php

--- web/header.php
+++ web/header.php
@@ -1,3 +1,7 @@
 <div class="jumbotron header">
     <h1>Welcome to my website!</h1>
+    <ul class="nav nav-pills nav-justified">
+        <li>About Me</li>
+        <li>My Assignments</li>
+    </ul>
 </div>
