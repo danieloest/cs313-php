f4405a4625547d3ce606a5d644c07793135f2ee8
danieloest <danieloest@users.noreply.github.com>
2019-05-03 23:08:26 -0600

    Adding pages

--- web/header.php
+++ web/header.php
@@ -1,7 +1,7 @@
 <div class="jumbotron header">
     <h1>Welcome to my website!</h1>
-    <ul class="nav nav-pills nav-justified">
-        <li>About Me</li>
-        <li>My Assignments</li>
+    <ul class="nav nav-tabs">
+        <li><a href="aboutMe.php">About Me</a></li>
+        <li><a href="index.php">My Assignments<a></li>
     </ul>
 </div>
