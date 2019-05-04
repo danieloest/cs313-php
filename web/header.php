a1c5d0fcfcc2af7e7283ec059ea738fb8b435d60
danieloest <danieloest@users.noreply.github.com>
2019-05-03 23:09:46 -0600

    Editing CSS

--- web/header.php
+++ web/header.php
@@ -1,6 +1,6 @@
 <div class="jumbotron header">
     <h1>Welcome to my website!</h1>
-    <ul class="nav nav-tabs">
+    <ul class="nav">
         <li><a href="aboutMe.php">About Me</a></li>
         <li><a href="index.php">My Assignments<a></li>
     </ul>
