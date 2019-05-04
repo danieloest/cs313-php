26491c72810fbe97894a69fbe85ab07e52bf42f5
danieloest <danieloest@users.noreply.github.com>
2019-05-04 17:21:10 -0600

    Adding Assignments

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <!-- Add Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="homePage.css">
+    <title>About Me</title>
 </head>
 <body>
-    
+    <?php
+    include 'header.php'
+    ?>
 </body>
 </html>
--- web/index.php
+++ web/index.php
@@ -13,5 +13,21 @@
   <?php
   include 'header.php'
   ?>
+  <ul>
+    <li><a href="hello.html">Assignment 01</a></li>
+    <li><a href="assign02.php">Assignment 02</a></li>
+    <li><a href="assign03.php">Assignment 03</a></li>
+    <li><a href="assign04.php">Assignment 04</a></li>
+    <li><a href="assign05.php">Assignment 05</a></li>
+    <li><a href="assign06.php">Assignment 06</a></li>
+    <li><a href="assign07.php">Assignment 07</a></li>
+    <li><a href="assign08.php">Assignment 08</a></li>
+    <li><a href="assign09.php">Assignment 09</a></li>
+    <li><a href="assign10.php">Assignment 10</a></li>
+    <li><a href="assign11.php">Assignment 11</a></li>
+    <li><a href="assign12.php">Assignment 12</a></li>
+    <li><a href="assign13.php">Assignment 13</a></li>
+    <li><a href="assign14.php">Assignment 14</a></li>
+  </ul>
 </body>
 </html>
