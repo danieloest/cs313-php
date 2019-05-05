b417d3cafa6e7f1e4dfa043dcf078f3b559b131e
danieloest <danieloest@users.noreply.github.com>
2019-05-04 18:37:47 -0600

    Fixing carousel

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -7,9 +7,9 @@
     <!-- Add Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="homePage.css">
-    <!-- Adding for testing carousel -->
+    <!-- Needed for carousel -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>About Me</title>
 </head>
 <body>
@@ -30,26 +30,26 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
     <div class="item active">
-      <img src="https://f4.bcbits.com/img/a3135933789_10.jpg" alt="Chania">
-      <div class="carousel-caption">
-        <h3>Los Angeles</h3>
-        <p>LA is always so much fun!</p>
+      <img src="https://f4.bcbits.com/img/a3135933789_10.jpg" alt="Landmark by Hippo Campus">
+      <div class="carousel-caption album">
+        <<h4>Landmark</h4>
+        <h5>Hippo Campus</h5>
       </div>
     </div>
 
     <div class="item">
-      <img src="https://snworksceo.imgix.net/ohi/a545ccb7-ca71-4c90-a60a-dd10421b4821.sized-1000x1000.jpg?w=1000" alt="Chicago">
-      <div class="carousel-caption">
-        <h3>Chicago</h3>
-        <p>Thank you, Chicago!</p>
+      <img src="https://snworksceo.imgix.net/ohi/a545ccb7-ca71-4c90-a60a-dd10421b4821.sized-1000x1000.jpg?w=1000" alt="Nothing Happens by Wallows">
+      <div class="carousel-caption album">
+        <h4>Nothing Happens</h4>
+        <h5>Wallows</h5>
       </div>
     </div>
 
     <div class="item">
-      <img src="https://img.apmcdn.org/f7761d321002ff6a223a28b8e636e5cc4621296c/uncropped/3bc86f-20181117-hippo-campus-bambi.jpg" alt="New York">
-      <div class="carousel-caption">
-        <h3>New York</h3>
-        <p>We love the Big Apple!</p>
+      <img src="https://img.apmcdn.org/f7761d321002ff6a223a28b8e636e5cc4621296c/uncropped/3bc86f-20181117-hippo-campus-bambi.jpg" alt="Bambi by Hippo Campus">
+      <div class="carousel-caption album">
+        <h4>Bambmi</h4>
+        <h5>Hippo Campus</h5>
       </div>
     </div>
   </div>
