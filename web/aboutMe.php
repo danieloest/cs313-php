6a97a1fc76bb941809250041b8dfb0e85fee64e5
danieloest <danieloest@users.noreply.github.com>
2019-05-04 18:32:05 -0600

    Fixing carousel

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -17,49 +17,49 @@
     <h2>My Favorite Ablums</h2>
     <!-- Slideshow of favorite ablums -->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-        <div class="item active">
-            <img src="https://f4.bcbits.com/img/a3135933789_10.jpg" alt="Landmark by Hippo Campus">
-            <div class="carousel-caption">
-                <h4>Landmark</h4>
-                <h5>Hippo Campus</h5>
-            </div>
-        </div>
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-        <div class="item">
-            <img src="https://snworksceo.imgix.net/ohi/a545ccb7-ca71-4c90-a60a-dd10421b4821.sized-1000x1000.jpg?w=1000" alt="Nothing Happens by Wallows">
-            <div class="carousel-caption">
-                <h4>Nothing Happens</h4>
-                <h5>Wallows</h5>
-            </div>
-        </div>
-
-        <div class="item">
-            <img src="https://img.apmcdn.org/f7761d321002ff6a223a28b8e636e5cc4621296c/uncropped/3bc86f-20181117-hippo-campus-bambi.jpg" alt="Bambi by Hippo Campus">
-            <div class="carousel-caption">
-                <h4>Bambi</h4>
-                <h5>Hippo Campus</h5>
-            </div>
-        </div>
-
-
-        <!-- Left and right controls -->
-        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            <span class="sr-only">Next</span>
-        </a>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="https://f4.bcbits.com/img/a3135933789_10.jpg" alt="Chania">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>LA is always so much fun!</p>
+      </div>
     </div>
+
+    <div class="item">
+      <img src="https://snworksceo.imgix.net/ohi/a545ccb7-ca71-4c90-a60a-dd10421b4821.sized-1000x1000.jpg?w=1000" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
     </div>
+
+    <div class="item">
+      <img src="https://img.apmcdn.org/f7761d321002ff6a223a28b8e636e5cc4621296c/uncropped/3bc86f-20181117-hippo-campus-bambi.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </body>
 </html>
