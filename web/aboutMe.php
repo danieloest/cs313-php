191dc1a33634a9b9fa270b67d3c5f278758d3b37
danieloest <danieloest@users.noreply.github.com>
2019-05-04 18:34:32 -0600

    Adding sources

--- web/aboutMe.php
+++ web/aboutMe.php
@@ -7,6 +7,9 @@
     <!-- Add Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="homePage.css">
+    <!-- Adding for testing carousel -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>About Me</title>
 </head>
 <body>
