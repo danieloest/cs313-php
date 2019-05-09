87b6ee9367b0fca5e7b510092c26bf5ecde0ed20
danieloest <danieloest@users.noreply.github.com>
2019-05-09 13:12:08 -0600

    Adding 'active' tab

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -1,13 +1,17 @@
+<?php
+    $activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+
 <div class="jumbotron">
     <h1 class="text-center">Welcome to ZMart!</h1>
     <div class="navbar">
         <ul class="nav nav-pills nav-justified">
-            <li>
-                <a href="browse.php">
-                    <h4>Home</h4>
-                </a>
-            </li>
-            <li>
+            <li id="home" class="<?= ($activePage == 'browse') ? 'active':''; ?>">
+                <a href=" browse.php">
+                    <h4>H ome</h4>
+                </a> 
+            </li> 
+            <li id="cart" class="<?= ($activePage == 'cart') ? 'active':''; ?>">
                 <a href="cart.php">
                     <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
                 </a>
