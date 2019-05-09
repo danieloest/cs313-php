99fce9769ea73507b4f3cbf5b2ac3af0002d8db5
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:26:43 -0600

    Fixed alignment issues for view cart

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -1,6 +1,6 @@
-<div class="jumbotron text-center">
-    <h1>Welcome to ZMart!</h1>
-        <div class="viewCart float-right">
+<div class="jumbotron">
+    <h1 class="text-center">Welcome to ZMart!</h1>
+        <div class="viewCart text-right">
             <a href="cart.php">
                 <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
             </a>
