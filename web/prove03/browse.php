7edd6f3b1857f07fe94113686941ca29b42ce21c
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:15:20 -0600

    Creating cart.php and adding link there

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -14,7 +14,9 @@ session_start();
     <div class="jumbotron text-center">
         <h1>Welcome to ZMart!</h1>
         <div class="viewCart">
-            <h5>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h5>
+            <a href="cart.php">
+                <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
+            </a>
         </div>
     </div>
 </body>
