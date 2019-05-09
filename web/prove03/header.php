179516bd223ba31986c694c6403738553a43a6e2
danieloest <danieloest@users.noreply.github.com>
2019-05-09 12:51:26 -0600

    Redoing header

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -1,13 +1,24 @@
 <div class="jumbotron">
     <h1 class="text-center">Welcome to ZMart!</h1>
     <div class="navbar">
-        <div class="sections">
+        <ul class="nav nav-pills nav-justified">
+            <li>
+                <a href="browse.php">
+                    <h4>Home</h4>
+                </a>
+            </li>
+            <a href="cart.php">
+                <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
+            </a>
+
+        </ul>
+        <!-- <div class="sections">
             <a class="nav-link" href="browse.php">Home</a>
         </div>
         <div class="viewCart text-right">
             <a href="cart.php">
                 <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
-            </a>
+            </a> -->
         </div>
     </div>
 </div>
