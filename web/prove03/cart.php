3d9ad8ea0b8dac53e13a0e45f41dc12408f0f6cb
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:28:05 -0600

    Added header for cart.php

---
+++ web/prove03/cart.php
@@ -0,0 +1,13 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ZMart</title>
+</head>
+<body>
+    <?php include 'header.php';?>
+</body>
+</html>
