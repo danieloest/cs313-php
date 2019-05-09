c1d952eeb794429d3b2390e9adc85a816f7ff3ed
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:13:41 -0600

    Changing bootstrap source to fix glyphicon issue

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -7,14 +7,14 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <title>ZMart</title>
 </head>
 <body>
     <div class="jumbotron text-center">
         <h1>Welcome to ZMart!</h1>
         <div class="viewCart">
-            <h5 class="glyphicon glyphicon-shopping-cart">View Cart</h5>
+            <h5>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h5>
         </div>
     </div>
 </body>
