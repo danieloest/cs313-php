14053fa3e1181f38562c3619572959f20bb48b13
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:34:05 -0600

    Adding 'Home' to navbar

--- web/prove03/header.php
+++ web/prove03/header.php
@@ -1,8 +1,13 @@
 <div class="jumbotron">
     <h1 class="text-center">Welcome to ZMart!</h1>
+    <div class="navbar">
+        <div class="sections">
+            <a class="nav-link" href="browse.php">Home</a>
+        </div>
         <div class="viewCart text-right">
             <a href="cart.php">
                 <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
             </a>
         </div>
+    </div>
 </div>
