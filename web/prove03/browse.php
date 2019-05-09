c1007f7b1f5a324ca27ac84378e6be79a3653b43
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:03:01 -0600

    Adding bootstrap

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -7,7 +7,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>ZMart</title>
 </head>
 <body>
     <div class="jumbotron text-center">
