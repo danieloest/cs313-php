d07b4042f82777f13210b1a1d074d139483c7bb9
danieloest <danieloest@users.noreply.github.com>
2019-05-09 11:24:49 -0600

    Adding header in seperate file and linking CSS sheet

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -8,16 +8,10 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="zmart.css">
     <title>ZMart</title>
 </head>
 <body>
-    <div class="jumbotron text-center">
-        <h1>Welcome to ZMart!</h1>
-        <div class="viewCart">
-            <a href="cart.php">
-                <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
-            </a>
-        </div>
-    </div>
+    <?php include 'header.php'; ?>
 </body>
 </html>
---
+++ web/prove03/header.php
@@ -0,0 +1,8 @@
+<div class="jumbotron text-center">
+    <h1>Welcome to ZMart!</h1>
+        <div class="viewCart float-right">
+            <a href="cart.php">
+                <h4>View Cart <span class="glyphicon glyphicon-shopping-cart"></span></h4>
+            </a>
+        </div>
+</div>
