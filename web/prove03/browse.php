e079c1b184336ee56ce522ec093a2f3100b50e52
danieloest <danieloest@users.noreply.github.com>
2019-05-11 16:49:41 -0600

    Displaying cart items

--- web/prove03/browse.php
+++ web/prove03/browse.php
@@ -1,4 +1,14 @@
-<?php session_start();
+<?php 
+    class Product {
+        function Product($cost, $quantity, $animal) {
+            $this->price = $cost;
+            $this->name = $animal;
+            $this->quantity = $quantity;
+
+        }
+    }
+
+session_start();
 
 // Add item to session
 $animal = $_GET['animal'];
@@ -9,14 +19,14 @@ if (!is_null($animal) && !is_null($quantity) && !is_null($cost)) {
     // var_dump($animal);
     // var_dump($quantity);
     // var_dump($cost);
-    class Product {
-        function Product($cost, $quantity, $animal) {
-            $this->price = $cost;
-            $this->name = $animal;
-            $this->quantity = $quantity;
+    // class Product {
+    //     function Product($cost, $quantity, $animal) {
+    //         $this->price = $cost;
+    //         $this->name = $animal;
+    //         $this->quantity = $quantity;
 
-        }
-    }
+    //     }
+    // }
     $product = new Product($cost, $quantity, $animal);
 
     echo "Product: " . $product->name . "<br>Quantity: " . $product->quantity . "<br>Price: " . $product->price;
