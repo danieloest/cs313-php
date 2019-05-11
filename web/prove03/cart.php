4ca7d25fc4d98374d0d26894f1a813443fded1b5
danieloest <danieloest@users.noreply.github.com>
2019-05-11 16:51:44 -0600

    Displaying cart items

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -45,7 +45,7 @@ session_start();?>
         //     echo "Item: $key -> $value <br>";  
         //   }
         for ($i = 0; i <= count($_SESSION); $i++) {
-            var_dump($_SESSION[$i]);
+            echo $_SESSION[$i]->product->name;
         }
     }
     ?>
