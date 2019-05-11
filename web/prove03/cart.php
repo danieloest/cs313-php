3e4915a3d6f8dba61a6eff675a899154e859917a
danieloest <danieloest@users.noreply.github.com>
2019-05-11 17:25:51 -0600

    styling table

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -29,7 +29,7 @@ session_start();?>
     }
     // If we have stuff in our cart
     else {
-        
+        $totalCost = 0;
         echo "<table class=\"table\">
         <tr>
         <th>Item</th>
@@ -38,21 +38,27 @@ session_start();?>
         </tr>";
         if(isset($_SESSION["Peppy Puppy"])) {
             echo "<tr><td>" . $_SESSION["Peppy Puppy"]->name . "</td><td>" . $_SESSION["Peppy Puppy"]->quantity . "</td><td>" . $_SESSION["Peppy Puppy"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Peppy Puppy"]->price;
         }
         if(isset($_SESSION["Krazy Kitty"])) {
             echo "<tr><td>" . $_SESSION["Krazy Kitty"]->name . "</td><td>" . $_SESSION["Krazy Kitty"]->quantity . "</td><td>" . $_SESSION["Krazy Kitty"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Krazy Kitty"]->price;
         }
         if(isset($_SESSION["Goat Galore"])) {
             echo "<tr><td>" . $_SESSION["Goat Galore"]->name . "</td><td>" . $_SESSION["Goat Galore"]->quantity . "</td><td>" . $_SESSION["Goat Galore"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Goat Galore"]->price;
         }
         if(isset($_SESSION["Crazy Cow"])) {
             echo "<tr><td>" . $_SESSION["Crazy Cow"]->name . "</td><td>" . $_SESSION["Crazy Cow"]->quantity . "</td><td>" . $_SESSION["Crazy Cow"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Crazy Cow"]->price;
         }
         if(isset($_SESSION["Blazing Birds"])) {
             echo "<tr><td>" . $_SESSION["Blazing Birds"]->name . "</td><td>" . $_SESSION["Blazing Birds"]->quantity . "</td><td>" . $_SESSION["Blazing Birds"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Blazing Birds"]->price;
         }
         if(isset($_SESSION["Geeky Gecko"])) {
             echo "<tr><td>" . $_SESSION["Geeky Gecko"]->name . "</td><td>" . $_SESSION["Geeky Gecko"]->quantity . "</td><td>" . $_SESSION["Geeky Gecko"]->price . "</td></tr>";
+            $totalCost += $_SESSION["Geek Gecko"]->price;
         }
 
 
