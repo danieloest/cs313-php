55c2932ee695a550b2f3b402aa81fe8f25f1f94d
danieloest <danieloest@users.noreply.github.com>
2019-05-11 18:11:16 -0600

    Adding check out button and page

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -84,7 +84,8 @@
             $totalCost += $_SESSION["Geek Gecko"]->price;
         }
         echo "<tr><td></td><td></td><td></td><td>$" . $totalCost . "</td></tr>";
-        echo "<tr><td></td><td></td><td></td><a href=\"checkOut.php\"><button type=\"button\" class=\"btn btn-primary pull-right checkOut\">Check out</button></a></tr></table>";
+        echo "<tr><td></td><td></td><td></td><td><a href=\"checkOut.php\"><button type=\"button\" class=\"btn btn-primary pull-right checkOut\">Check out</button></a></td></tr>";
+        
 
     }
     ?>
