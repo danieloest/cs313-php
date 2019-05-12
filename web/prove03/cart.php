b646f1864991a1a2d1c996f86a8716395e347e7c
danieloest <danieloest@users.noreply.github.com>
2019-05-11 18:16:06 -0600

    Adding check out button and page

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -86,7 +86,6 @@
         echo "<tr><td></td><td></td><td></td><td>$" . $totalCost . "</td></tr>";
         echo "<tr><td></td><td></td><td></td><td><a href=\"checkOut.php\"><button type=\"button\" class=\"btn btn-primary pull-right checkOut\">Check out</button></a></td></tr></table>";
         
-
     }
     ?>
 </body>
