428b6d32b89b2bd0c2d4abf951a77f69b4c02269
danieloest <danieloest@users.noreply.github.com>
2019-05-11 22:41:43 -0600

    Working on updating quantity in cart

--- web/prove03/cart.php
+++ web/prove03/cart.php
@@ -64,7 +64,7 @@
             $totalCost += $_SESSION["Peppy Puppy"]->price;
         }
         if(isset($_SESSION["Krazy Kitty"])) {
-            echo "<tr><td>" . $_SESSION["Krazy Kitty"]->name . "</td><td><div><input class=\"inputNum\" type=\"number\" value=\"" . $_SESSION["Krazy Kitty"]->quantity . "\" id=\"" . $_SESSION["Krazy Kitty"]->name . "quantity\"><p onclick=\"update()\">✔<p></div></td><td><a href=\"cart.php?remove=PP\">❌</a></td><td>$" . $_SESSION["Krazy Kitty"]->price . "</td></tr>";
+            echo "<tr><td>" . $_SESSION["Krazy Kitty"]->name . "</td><td><div class=\"flex-container\"><input class=\"inputNum\" type=\"number\" value=\"" . $_SESSION["Krazy Kitty"]->quantity . "\" id=\"" . $_SESSION["Krazy Kitty"]->name . "quantity\"><p onclick=\"update(" . $_SESSION["Krazy Kitty"]->name . "quantity\")\">✔<p></div></td><td><a href=\"cart.php?remove=PP\">❌</a></td><td>$" . $_SESSION["Krazy Kitty"]->price . "</td></tr>";
             $totalCost += $_SESSION["Krazy Kitty"]->price;
         }
         if(isset($_SESSION["Goat Galore"])) {
@@ -89,8 +89,8 @@
     }
     ?>
     <script>
-        function update() {
-            alert("In update");
+        function update(quantity) {
+            alert("In update. You are updating to: " + quantity);
         }
     </script>
 </body>
