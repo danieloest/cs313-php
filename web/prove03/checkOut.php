18b54339830498311ba7d03d9557ee40938ab885
danieloest <danieloest@users.noreply.github.com>
2019-05-11 18:59:17 -0600

    Adding check out button and page

--- web/prove03/checkOut.php
+++ web/prove03/checkOut.php
@@ -14,22 +14,22 @@
     <form action="confirm.php">
         <div class="form-group">
             <label for="fName">First Name</label>
-            <input class="form-control" type="text" name="fName" id="fName" placeholder="Enter your first name">
+            <input class="form-control smallInput" type="text" name="fName" id="fName" placeholder="Enter your first name">
         </div>
         <div class="form-group">
             <label for="fLame">Last Name</label>
-            <input class="form-control" type="text" name="lName" id="lName" placeholder="Enter your last name">
+            <input class="form-control smallInput" type="text" name="lName" id="lName" placeholder="Enter your last name">
         </div>
         <div class="form-group">
             <label for="email">Email</label>
-            <input class="form-control" type="email" name="emil" id="emil" placeholder="Enter your email">
+            <input class="form-control smallInput" type="email" name="emil" id="emil" placeholder="Enter your email">
         </div>
         <div class="form-group">
             <label for="street">Address</label>
-            <input class="form-control" type="text" name="street" id="street" placeholder="Street">
-            <input class="form-control" type="text" name="city" id="city" placeholder="City">
-            <input class="form-control" type="text" name="state" id="state" placeholder="St" size="2">
-            <input class="form-control" type="text" name="zipCode" id="zipCode" placeholder="Zip" size="5">
+            <input class="form-control smallInput" type="text" name="street" id="street" placeholder="Street">
+            <input class="form-control smallInput" type="text" name="city" id="city" placeholder="City">
+            <input class="form-control xsmallInput" type="text" name="state" id="state" placeholder="St" size="2">
+            <input class="form-control mediumInput" type="text" name="zipCode" id="zipCode" placeholder="Zip" size="5">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
