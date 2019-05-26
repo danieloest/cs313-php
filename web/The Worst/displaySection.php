b76ef8fdc867e56b675678965b17a2d75d5e2706
danieloest <danieloest@users.noreply.github.com>
2019-05-25 23:05:00 -0600

    Fixing php

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -7,6 +7,7 @@
     foreach ($rows as $clothing) {
         echo '<div class="container">';
         echo '<div class=imgContainer><img src="' . $clothing['previewpicture'] . '"></div>';
+        echo '<div><p class="name">' . $clothing['clothingname'] . ' ' . $clothing['price'] . '</p></div>';
         echo '</div>';
         // echo "We are looking at: " . $clothing['clothingname'] . ". For $" . $clothing['price'];
         // echo '<img src="' . $clothing['previewpicture'] . '">';
