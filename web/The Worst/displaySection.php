7914f682b98fa813de4a5fa641272adffd8e9882
danieloest <danieloest@users.noreply.github.com>
2019-05-25 23:40:13 -0600

    Fixing css

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -8,7 +8,7 @@
         echo '<div class="container"><a href=clothing.php?id="' . $clothing['clothingid'] . '">';
         echo '<div class=imgContainer><img src="' . $clothing['previewpicture'] . '"></div>';
         echo '<div><p class="name">' . $clothing['clothingname'] . '   $' . $clothing['price'] . '</p></div>';
-        echo '<button type="button" class="btn btn-default btn-sm">Add to Cart<span class="glyphicon glyphicon-plus"></span> Plus</button>';
+        echo '<button type="button" class="btn btn-default btn-sm">Add to Cart  <span class="glyphicon glyphicon-plus"></span></button>';
         echo '</a></div>';
         // echo "We are looking at: " . $clothing['clothingname'] . ". For $" . $clothing['price'];
         // echo '<img src="' . $clothing['previewpicture'] . '">';
