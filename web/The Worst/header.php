762da9cec64c58133365866544819c208d2a24c4
danieloest <danieloest@users.noreply.github.com>
2019-05-25 22:48:14 -0600

    Fixing php

--- web/The Worst/header.php
+++ web/The Worst/header.php
@@ -10,7 +10,7 @@
                                         <?php 
                                         // Display the sections. Query from database
                                         foreach ($db->query('SELECT sectionname, sectionid  FROM clothingsection') as $clothing) {
-                                                echo '<li><a href="section.php?section=' . $clothing['sectionid'] . '"/a>'. $clothing['sectionname'] .'</li>';
+                                                echo '<li><a href="section.php?section=' . $clothing['sectionid'] . '">'. $clothing['sectionname'] .'</a></li>';
                                         }
                                         ?>
                                 </ul>
