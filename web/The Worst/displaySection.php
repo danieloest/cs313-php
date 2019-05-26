93ee06ca94b837917f79cde65ed1b882a8fd2f07
danieloest <danieloest@users.noreply.github.com>
2019-05-25 22:46:59 -0600

    Fixing php

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -1,7 +1,7 @@
 <?php
     $section = $_GET['section'];
     // echo 'We are looking for: ' . $section;
-    $stmt = $db->prepare('SELECT price, previewPicture, clothingName FROM clothing WHERE clothingsection=:section');
+    $stmt = $db->prepare('SELECT clothingName, price, previewPicture FROM clothing WHERE clothingsection=:section');
     $stmt->execute(array(':section' => $section));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($rows as $clothing) {
