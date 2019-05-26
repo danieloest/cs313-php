ba06f1aab840415f27b2f5d329c7230d8f9d3f73
danieloest <danieloest@users.noreply.github.com>
2019-05-25 21:58:44 -0600

    Adding pills to header

--- web/The Worst/header.php
+++ web/The Worst/header.php
@@ -8,23 +8,25 @@
                         <ul class="dropdown-menu">
                                 <?php 
                                 // Display the sections. Query from database
-                                $stmt = $db->prepare('SELECT * FROM clothingsection');
-                                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                             
-                             
-                                foreach ($rows as $scripture) {
+                                foreach ($db->query('SELECT sectionname password FROM clothingsection') as $row)
+                                {
                                         echo "<li><a href=\"http://\"></a>". $rows['sectionname'] ."</li>";
-                                //    echo "<b><a href=\"ta05.php?id=" . $scripture['id'] . "\">" . $scripture['book'] . " " . $scripture['chapter'] . ": " . $scripture['verse'] . "</b></a>";
-                                //    echo '<br/>';
                                 }
+
+
+
+                                // $stmt = $db->prepare('SELECT * FROM clothingsection');
+                                // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                             
+                             
+                                // foreach ($rows as $scripture) {
+                                //         echo "<li><a href=\"http://\"></a>". $rows['sectionname'] ."</li>";
+                                // //    echo "<b><a href=\"ta05.php?id=" . $scripture['id'] . "\">" . $scripture['book'] . " " . $scripture['chapter'] . ": " . $scripture['verse'] . "</b></a>";
+                                // //    echo '<br/>';
+                                // }
                                 ?>
-                                <!-- <li><a href="#">Shirts</a></li>
-                                <li><a href="#">Submenu 1-2</a></li>
-                                <li><a href="#">Submenu 1-3</a></li>  -->
                         </ul>
                  </li>
-                <li><a href="#">Menu 2</a></li>
-                <li><a href="#">Menu 3</a></li>
         </ul>
 </div>
 
