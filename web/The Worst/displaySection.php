d0d259e363e51811a24f7712d15f76771bbb674c
danieloest <danieloest@users.noreply.github.com>
2019-05-25 23:06:47 -0600

    Fixing php

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -1,14 +1,14 @@
 <?php
     $section = $_GET['section'];
     // echo 'We are looking for: ' . $section;
-    $stmt = $db->prepare('SELECT clothingname, price, previewpicture FROM clothing WHERE clothingsection=:section');
+    $stmt = $db->prepare('SELECT clothingname, price, previewpicture, clothingid FROM clothing WHERE clothingsection=:section');
     $stmt->execute(array(':section' => $section));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($rows as $clothing) {
-        echo '<div class="container">';
+        echo '<div class="container"><a href=">' . $clothing['clothingid'] . '">';
         echo '<div class=imgContainer><img src="' . $clothing['previewpicture'] . '"></div>';
         echo '<div><p class="name">' . $clothing['clothingname'] . ' ' . $clothing['price'] . '</p></div>';
-        echo '</div>';
+        echo '</a></div>';
         // echo "We are looking at: " . $clothing['clothingname'] . ". For $" . $clothing['price'];
         // echo '<img src="' . $clothing['previewpicture'] . '">';
 
