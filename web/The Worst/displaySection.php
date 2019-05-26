73a097727c4c7bc21c8229320f92b74d1117fe28
danieloest <danieloest@users.noreply.github.com>
2019-05-25 22:40:23 -0600

    Fixing php

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -1,4 +1,12 @@
 <?php
     $section = $_GET['section'];
-    echo 'We are looking for: ' . $section;
+    // echo 'We are looking for: ' . $section;
+    $stmt = $db->prepare('SELECT price, previewPicture, clothingName FROM clothing WHERE clothingsection=:section');
+    $stmt->execute(array(':section' => $_GET[$section]));
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($rows as $clothing) {
+        echo "We are looking at: " . $clothing['clothingName'] . ". For $" . $clothing['price'];
+        echo '<img src="' . $clothing['previewPicture'] . '">';
+
+    }
 ?>
--- web/The Worst/header.php
+++ web/The Worst/header.php
@@ -2,7 +2,7 @@
         <h1 class="title">The Worst</h1>
         <div class="menu-flex">
                 <ul class="nav nav-tabs">
-                        <li class="active"><a href="home.php">Home</a></li>
+                        <li><a href="home.php">Home</a></li>
                         <li class="dropdown">
                                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">Clothing Sections
                                 <span class="caret"></span></a>
