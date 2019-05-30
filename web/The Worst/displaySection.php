ec3e4f1d817f3f9889db46fb2be472b152cc236c
danieloest <danieloest@users.noreply.github.com>
2019-05-30 15:29:12 -0600

    Fixing displaySection

--- web/The Worst/displaySection.php
+++ web/The Worst/displaySection.php
@@ -1,12 +1,12 @@
 <?php
     $section = $_GET['section'];
-    $stmt = $db->prepare('SELECT clothingname, price, previewpicture, clothingid FROM product WHERE clothingsection=:section');
+    $stmt = $db->prepare('SELECT productname, price, previewpicture, productid FROM product WHERE clothingsection=:section');
     $stmt->execute(array(':section' => $section));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach ($rows as $clothing) {
-        echo '<div class="container"><a href=clothing.php?id=' . $clothing['clothingid'] . '>';
+        echo '<div class="container"><a href=clothing.php?id=' . $clothing['productid'] . '>';
         echo '<div class=imgContainer><img src="' . $clothing['previewpicture'] . '"></div>';
-        echo '<div><p class="name">' . $clothing['clothingname'] . '   $' . $clothing['price'] . '</p></div>';
+        echo '<div><p class="name">' . $clothing['productname'] . '   $' . $clothing['price'] . '</p></div>';
         echo '<button type="button" class="btn btn-default btn-sm">Add to Cart  <span class="glyphicon glyphicon-plus"></span></button>';
         echo '</a></div>';
 
