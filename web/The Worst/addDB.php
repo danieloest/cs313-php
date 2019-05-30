ba0136d3e777fd0e8f1a129a0f664e2f5d4b727d
danieloest <danieloest@users.noreply.github.com>
2019-05-30 16:25:41 -0600

    Adding php to insert into database

--- web/The Worst/addDB.php
+++ web/The Worst/addDB.php
@@ -16,7 +16,12 @@
     echo $section . '<br>';
 
     $statement  = $db->prepare("INSERT INTO product(name, mainPicture, previewPicture, sidePicture, price, section) VALUES (:name, :mainPicutre, :previewPicture, :sidePicture, :price, :section);");
-    $statement->execute(array(':name' => $name, ':mainPicture' => $mainPicture, ':previewPicture' => $previewPicture,':sidePicture' => sidePicture, ':price' => price, ':section' => $section));
+    // $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+    // $stmt->bindValue(':mainPicture', $mainPicture, PDO::PARAM_STR);
+    // $stmt->bindValue(':previewPicture', $previewPicture, PDO::PARAM_STR);
+    // $stmt->bindValue(':sidePicture', $sidePicture, PDO::PARAM_STR);
+    // $stmt->bindValue(':price', $price, PDO::??);
+    $statement->execute(array(':name' => $name, ':mainPicture' => $mainPicture, ':previewPicture' => $previewPicture,':sidePicture' => $sidePicture, ':price' => $price, ':section' => $section));
 
     
     ?>
