cc0da12291c22d5534db4cef98264e7bb32fac4d
danieloest <danieloest@users.noreply.github.com>
2019-05-30 16:36:38 -0600

    Adding php to insert into database

--- web/The Worst/addDB.php
+++ web/The Worst/addDB.php
@@ -17,8 +17,8 @@
     var_dump($section);
     echo $section . '<br>';
 
-    // $statement  = $db->prepare("INSERT INTO product(productname, mainpicture, previewpicture, sidepicture, price, clothingsection) VALUES (:productname, :mainPicutre, :previewPicture, :sidePicture, :price, :clothingsection);");
-    $statement  = $db->prepare("INSERT INTO product(:productname, :mainPicutre, :previewPicture, :sidePicture, :price, :clothingsection);");
+    $statement  = $db->prepare("INSERT INTO product(productname, mainpicture, previewpicture, sidepicture, price, clothingsection) VALUES (:productname, :mainPicutre, :previewPicture, :sidePicture, :price, :clothingsection);");
+    // $statement  = $db->prepare("INSERT INTO product(:productname, :mainPicutre, :previewPicture, :sidePicture, :price, :clothingsection);");
     // $stmt->bindValue(':productname', $name, PDO::PARAM_STR);
     // $stmt->bindValue(':mainpicture', $mainPicture, PDO::PARAM_STR);
     // $stmt->bindValue(':previewpicture', $previewPicture, PDO::PARAM_STR);
@@ -26,7 +26,7 @@
     // $stmt->bindValue(':price', $price, PDO::PARAM_STR);
     // $stmt->bindValue(':clothingsection', $price, PDO::PARAM_STR);
 
-    $statement->execute(array(':productname' => $name, ':mainpicture' => $mainpicture, ':preview[icture' => $previewPicture,':sidePicture' => $sidePicture, ':price' => $price, ':clothingsection' => $section));
+    $statement->execute(array(':productname' => $name, ':mainpicture' => $mainPicture, ':preview[icture' => $previewPicture,':sidePicture' => $sidePicture, ':price' => $price, ':clothingsection' => $section));
 
     
     ?>
