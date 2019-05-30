27dc9c3b043635711f2f111702068379b19177f2
danieloest <danieloest@users.noreply.github.com>
2019-05-30 16:54:23 -0600

    Adding php to insert into database

--- web/The Worst/addDB.php
+++ web/The Worst/addDB.php
@@ -17,7 +17,7 @@
     var_dump($section);
     echo $section . '<br>';
 
-    $statement  = $db->prepare("INSERT INTO product (productname, mainpicture, previewpicture, sidepicture, price, clothingsection) VALUES (:productname, :mainpicutre, :previewpicture, :sidepicture, :price, :clothingsection);");
+    $stmt  = $db->prepare("INSERT INTO product (productname, mainpicture, previewpicture, sidepicture, price, clothingsection) VALUES (:productname, :mainpicutre, :previewpicture, :sidepicture, :price, :clothingsection);");
     // $statement->execute(array(':productname' => $name, ':mainpicture' => $mainpicture, ':previewpicture' => $previewpicture,':sidepicture' => $sidepicture, ':price' => $price, ':clothingsection' => $section));
     // $statement  = $db->prepare("INSERT INTO product(:productname, :mainPicutre, :previewPicture, :sidePicture, :price, :clothingsection);");
     $stmt->bindParam(':productname', $name);
