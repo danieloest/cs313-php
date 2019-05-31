e55e7d5c091d11ffc2237d80ec636802824a0729
danieloest <danieloest@users.noreply.github.com>
2019-05-31 16:51:48 -0600

    Trying to get AJAX to work

--- web/The Worst/edit.php
+++ web/The Worst/edit.php
@@ -1,22 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="worst.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>The Worst</title>
 </head>
 <body>
     <?php
     include 'connect.php';
     include 'header.php';
     ?>
-    <select name="" id="">
+    <select name="" id="selector" onchange="loadCategory()">
         <?php
         foreach ($db->query('SELECT sectionname, sectionid  FROM clothingsection') as $clothing) {
             echo '<option value="' . $clothing['sectionid'] . '">'. $clothing['sectionname'] .'</option>';
         }
         ?>
     </select>
+    <script>
+    function loadCategory() {
+        alert("in load category");
+        var e = document.getElementById("selector");
+        var value = e.options[e.selectedIndex].value;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            // document.getElementById("demo").innerHTML = this.responseText;
+            console.log(this.responseText);
+            }
+        };
+        xhttp.open("GET", "loadCategories.php", true);
+        xhttp.send(value);
+    }
+    </script>
 </body>
 </html>
---
+++ web/The Worst/loadCategories.php
@@ -0,0 +1,5 @@
+<?php
+include 'connect.php';
+$category = $_GET['value'];
+}
+?>
