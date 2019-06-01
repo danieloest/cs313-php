a62d57f025445b9a6390450f1c0311df366e796f
danieloest <danieloest@users.noreply.github.com>
2019-05-31 21:51:18 -0600

    Trying to get AJAX to work

--- web/The Worst/edit.php
+++ web/The Worst/edit.php
@@ -30,7 +30,7 @@
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
             // document.getElementById("demo").innerHTML = this.responseText;
-                e.appendChild(echo);
+                e.appendChild(this.responseText);
                 console.log(this.responseText);
             }
         };
