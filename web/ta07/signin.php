814274bf212d52a39947b1d972a1f54be3d6aa1b
danieloest <danieloest@users.noreply.github.com>
2019-06-04 16:13:49 -0600

    Adding AJAX request

--- web/ta07/signin.php
+++ web/ta07/signin.php
@@ -20,7 +20,7 @@
     function login() {
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        var paremeters = 'username="' . username . '"&password="' . password . '"';
+        var paremeters = 'username="' + username + '"&password="' + password + '"';
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
