653c38999b100d7991cc322cc16767a8897fbcc3
danieloest <danieloest@users.noreply.github.com>
2019-06-04 17:11:57 -0600

    Adding AJAX request

--- web/ta07/loginUser.php
+++ web/ta07/loginUser.php
@@ -3,17 +3,14 @@ include 'connect.php';
 $username = $_POST['username'];
 // $pass = password_hash($_POST['password'], $dbPassword);
 
-$query = ' SELECT username, pass FROM usersteam WHERE username=:username';
+$query = 'SELECT username, pass FROM usersteam WHERE username=:username';
 $statement = $db->prepare($query);
 $statement->bindValue(':username', $username);
 $statement->execute();
 $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 header('Content-Type: application/json');
-// $test = "{'response': '!!!!'}";
-// echo json_encode($test);
 foreach ($rows as $row) {
-        // $pass = password_verify($_POST['password'], $dbPassword);
-        if (password_verify($pass, $row['password']))
+        if (password_verify($pass, $row['pass']))
         {
             $data = "{'response': 'Success'}";
         }
