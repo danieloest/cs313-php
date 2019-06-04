cfe098a178e3f303f66ece62214dce37897d55eb
danieloest <danieloest@users.noreply.github.com>
2019-06-04 16:37:23 -0600

    Adding AJAX request

--- web/ta07/signin.php
+++ web/ta07/signin.php
@@ -24,8 +24,9 @@
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                var response = JSON.stringify(this.responseText);
-                console.log(response);
+                console.log("original response: " + this.resposneText);
+                var response = JSON.parse(this.responseText);
+                console.log("\nParsed: " + response);
                 // Handle if success or not
             }
         };
