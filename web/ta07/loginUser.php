4fb2b8c96ec2b30681f84a396e4ebecb1bf19462
danieloest <danieloest@users.noreply.github.com>
2019-06-04 16:01:16 -0600

    Fixing spelling error

--- web/ta07/loginUser.php
+++ web/ta07/loginUser.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 $username = $_POST['username'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$query = 'SELECT username FROM usersTeam(username, password) WHERE username=:username AND password=:password';
+$query = 'SELECT username FROM usersTeam WHERE username=:username AND password=:password;';
 $statement = $db->prepare($query);
 $statement->bindValue(':username', $username);
 $statement->bindValue(':password', $password);
