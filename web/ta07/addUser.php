38e37b4c2ebfc7f10766ffb731ce4622a29e0b9d
danieloest <danieloest@users.noreply.github.com>
2019-06-05 12:28:48 -0600

    Working on stretch 1

--- web/ta07/addUser.php
+++ web/ta07/addUser.php
@@ -2,9 +2,18 @@
 include 'connect.php';
 $username = $_POST['username'];
 $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$query = 'INSERT into usersTeam(username, pass) VALUES (:username, :pass)';
-$statement = $db->prepare($query);
-$statement->bindValue(':username', $username);
-$statement->bindValue(':pass', $pass);
-$statement->execute();
+$passConfirm = password_hash($_POST['passwordConfirm'], PASSWORD_DEFAULT);
+if ($pass != $passConfirm)
+{
+    header('Location: signup.php?error=1');
+    die();
+}
+else 
+{
+    $query = 'INSERT into usersTeam(username, pass) VALUES (:username, :pass)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':pass', $pass);
+    $statement->execute();
+}
 ?>
