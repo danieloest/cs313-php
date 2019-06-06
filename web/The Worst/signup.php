eed78ec0a9d6312b79f0990737f68ccf946f084c
danieloest <danieloest@users.noreply.github.com>
2019-06-06 15:28:12 -0600

    Adding login and signup pages

--- web/The Worst/signup.php
+++ web/The Worst/signup.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="worst.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>The Worst</title>
 </head>
 <body>
-    
+    <?php include 'connect.php';?>
+    <?php include 'header.php';?>
 </body>
 </html>
