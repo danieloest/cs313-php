576c8861cd4bd23fb43f64f5e6bed4d87b341f3c
danieloest <danieloest@users.noreply.github.com>
2019-06-06 16:34:54 -0600

    Working on sign up

---
+++ web/The Worst/addUser.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include 'connect.php';
+$username = $_POST['username'];
+$pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$passConfirm = $_POST['passwordConfirm'];
+$passOriginal = $_POST['password'];
+if ($passOriginal != $passConfirm)
+{
+    header('Location: signup.php?error=1');
+    die();
+}
+else if (strlen($passOriginal) < 7 || !preg_match('~[0-9]~', $passOriginal))
+{
+    header('Location: signup.php?error=2');
+    die();
+}
+else 
+{
+    $query = 'INSERT into usersTeam(username, pass) VALUES (:username, :pass, false)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':pass', $pass);
+    $statement->bindValue('false', false);
+    $statement->execute();
+    $_SESSION["username"] = $username;
+    header('Location: home.php');
+}
+?>
