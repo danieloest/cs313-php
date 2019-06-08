ce82cd375c479b5ccc408fa88478e8ae1b96a17b
danieloest <danieloest@users.noreply.github.com>
2019-06-07 20:42:57 -0600

    Adding loginUser.php

---
+++ web/The Worst/loginUser.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include 'connect.php';
+$username = $_POST['username'];
+// $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$pass = $_POST['password'];
+
+$statement = $db->prepare('SELECT username, pass FROM usersteam WHERE username=:username;');
+$statement->bindValue(':username', $username);
+$statement->execute();
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json');
+foreach ($rows as $row) {
+    if (password_verify($pass, $row['pass']))
+    {
+        $_SESSION["username"] = $username;
+        $data = "Success";
+    }
+    else
+    {
+        $data = "Incorrect username/password";
+    }   
+}
+echo json_encode($data);
+
+?>
