d3f810a379b44648c971d68c2d7e1d78cb032889
danieloest <danieloest@users.noreply.github.com>
2019-06-07 20:33:34 -0600

    Working on login

--- web/The Worst/login.php
+++ web/The Worst/login.php
@@ -13,6 +13,43 @@
 <body>
     <?php include 'connect.php';?>
     <?php include 'header.php';?>
-    
+    <form action="#" method="" onsubmit="return false">
+    <label for="username">User Name</label>
+    <input type="text" name="username" id="username">
+    <br>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <button onclick="login()">Sign in</button>
+    </form>
+    <p id="messageBox"></p>
+    <a href="signup.php"><p>Not a member? Sign up here</p></a>
+    <script>
+    function login() {
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        var parameters = 'username=' + username + '&password=' + password;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log("original response: " + this.responseText);
+                var response = JSON.parse(this.responseText);
+                if (response == "Success")
+                {
+                    window.location = "home.php";
+                }
+                else
+                {
+
+                    document.getElementById('messageBox').innerHTML = response;
+                }
+                console.log("\nParsed: " + response);
+            }
+        };
+        xhttp.open("POST", "loginUser.php", true);
+        // Needs the line below in order to send parameters. This specifies that it is sending data to be used.
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send(parameters);
+    }
+    </script>
 </body>
 </html>
